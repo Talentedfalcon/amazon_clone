71f80d7d9e3b662f6fe1254c69c446b19d91f08c
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2024-05-22 00:20:45 +0530

    small changes

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = localStorage.getItem('cart') || [];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart));
