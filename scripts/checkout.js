f8adbe93d9812dc575f5fda0aad3acac8cf3e244
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2024-05-22 00:04:28 +0530

    delete feature complete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach(
             }
         })
         cartSummaryHTML+=`
-                <div class="cart-item-container">
+                <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
                     <div class="delivery-date">
                         Delivery date: Tuesday, June 21
                     </div>
@@ -101,7 +101,8 @@ document.querySelectorAll('.delete-quantity-link').forEach(
         link.addEventListener('click',()=>{
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            console.log(cart);
+            const container=document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         })
     }
 )
