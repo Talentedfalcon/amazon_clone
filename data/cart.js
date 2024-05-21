0db66e606400412057437421c80a3a2b66da27ac
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2024-05-22 00:14:31 +0530

    cart now saves

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,8 @@
-export let cart = [
-    {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 3
-    },
-    {
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 2
-    }
-];
+export let cart = localStorage.getItem('cart') || [];
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId,productCount)
 {
@@ -30,7 +25,7 @@ export function addToCart(productId,productCount)
         });
     }
 
-    // localStorage.setItem('cart',JSON.stringify(cart));
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -41,4 +36,5 @@ export function removeFromCart(productId){
         }
     });
     cart=newCart;
+    saveToStorage();
 }
